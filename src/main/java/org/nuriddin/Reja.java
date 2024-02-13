72574e47a1a4ed42c23829ea4ffcda0ee7b390da
nuriddin5 <nuriddinqurbonboyev@mail.ru>
2024-02-13 07:03:18 +0500

    Reja done

---
+++ src/main/java/org/nuriddin/Reja.java
@@ -0,0 +1,22 @@
+package org.nuriddin;
+
+/**
+ * User:t.me/supermatematikuz
+ * Date:13.02.2024 6:49
+ */
+public class Reja {
+    //todo Collections in Java
+    //4 - modul 18-19-dars
+    //links https://www.javatpoint.com/collections-in-java
+    //https://www.w3schools.com/java/java_arraylist.asp
+
+    //todo Aggregation, Association, Composition haqida tushuncha av kodlar
+    //Links : https://www.geeksforgeeks.org/association-composition-aggregation-java/
+    //https://www.baeldung.com/java-composition-aggregation-association
+
+    //todo Java keyboards
+    //chatGpt vs https://core.telegram.org/bots/api
+
+
+
+}
