d803dc4881f08af11bbb71b33a45c5fca00b4b07
nuriddin5 <nuriddinqurbonboyev@mail.ru>
2024-02-13 11:54:02 +0500

    Arraylist exercises done

--- src/main/java/org/nuriddin/Main.java
+++ src/main/java/org/nuriddin/Main.java
@@ -2,6 +2,7 @@ package org.nuriddin;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -39,10 +40,39 @@ public class Main {
         //1) oila a'zolaringiz ismini o'z ichiga olgan arraylist oching va
         // uni iteratsiya qilib qiymatlarni chiqaring
 
+        ArrayList<String> oilaList = new ArrayList<>();
+        oilaList.add("Aliy");
+        oilaList.add("Fotima");
+        oilaList.add("Hasan");
+        oilaList.add("Husayn");
+
+        for (String azo : oilaList) {
+            System.out.print(azo + " ");
+        }
+
         //2) Ismlarni bosh harflari va oxirgi harflarini chiqaring
+
+        for (String azo : oilaList) {
+            System.out.println(azo + " 1 - "+ azo.charAt(0) + " 2 - " + azo.charAt(azo.length() - 1));
+        }
 
         //3) OilaAzo deb atalgan class oching, ism va qarindoshlik,
         //object yaratib ularni listga oling va chiqaring
+
+        OilaAzo ota = new OilaAzo("Aliy", "Ota");
+        OilaAzo ona = new OilaAzo("Fotima", "Ona");
+        OilaAzo aka = new OilaAzo("Hasan", "O'g'il");
+        OilaAzo uka = new OilaAzo("Husayn", "O'g'il");
+        ArrayList<OilaAzo> oila = new ArrayList<>(
+                List.of(ota, ona, uka, aka)
+        );
+//        oila.addAll(List.of(ota, ona, uka, aka));
+
+        for (OilaAzo oilaAzo : oila) {
+            System.out.println(oilaAzo.getQarishdoshlik() + " : " + oilaAzo.getIsm());
+        }
+
+
 
     }
 }
---
+++ src/main/java/org/nuriddin/OilaAzo.java
@@ -0,0 +1,18 @@
+package org.nuriddin;
+
+import lombok.*;
+
+/**
+ * User:t.me/supermatematikuz
+ * Date:13.02.2024 11:39
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class OilaAzo {
+    private String ism;
+    private String qarishdoshlik;
+}
